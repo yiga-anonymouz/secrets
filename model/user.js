caa613e1d67a2003b7a4fc566761e69ee686cee9
yiga-anonymouz <idrisadeyiga@gmail.com>
2023-01-02 23:38:23 +0100

    Added gitignore and .env file

--- model/user.js
+++ model/user.js
@@ -1,15 +1,14 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 const encrypt = require('mongoose-encryption')
-const { use } = require('../routes/routes')
+
 
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 })
 
-const secret = "Thisisourlittlesecret"
-
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']})
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']})
 
 const Users = new mongoose.model('user', userSchema)
 
